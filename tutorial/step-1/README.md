5b785d82d813c1c24623c35e9f528e9d9584130e
kianenigma <kian@parity.io>
2023-05-31 12:07:04 +0200

    revive step1

---
+++ tutorial/step-1/README.md
@@ -0,0 +1,32 @@
+# Step 1
+
+In this step we will build a very very simple currency pallet, that can mint and trasnfer tokens.
+
+- [Step 1](#step-1)
+	- [information Points](#information-points)
+		- [State Transition, Background Knowledge](#state-transition-background-knowledge)
+		- [In the code](#in-the-code)
+
+
+## information Points
+
+ The "potential" data-poinst to be covered are the following.
+
+### State Transition, Background Knowledge
+
+* Brief intro about what total issuance is, although it is pretty self-explanatory.
+* Technically you want `mint` to be permissioned, but for now we don't have the tools, so anyone can mint anything.
+
+
+### In the code
+
+* `frame::prelude`, followed by `use super::*`. A very common pattern.
+* `#[pallet::storage]` declares a type as *state*. For now, we only know
+  * Mapping.
+  * Single value.
+  * `ValueQuery` vs `OptionQuery`. (TODO: imporve RustDocs).
+  * How to navigate the storage APIs. No need to explain every single one, just point them to the right rust-docs.
+  * eg. `get`, `put`, `insert` and `mutate` has been used.
+
+* Where is `T::AccountId` coming from? better spend some time on this now and explain it well.
+* As noted in step0, `Err("AnyString".into())` is acceptable for `DispatchResult`.
---
+++ tutorial/step-1/frame-pallets/src/currency.rs
@@ -0,0 +1,51 @@
+use frame::prelude::*;
+
+#[frame::pallet(dev_mode)]
+pub mod pallet {
+	use super::*;
+
+	pub type Balance = u128;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {}
+
+	/// Mapping from account ID to balance.
+	#[pallet::storage]
+	pub type Balances<T: Config> = StorageMap<_, _, T::AccountId, Balance>;
+
+	/// Sum of all the tokens in existence.
+	#[pallet::storage]
+	pub type TotalIssuance<T: Config> = StorageValue<_, Balance, ValueQuery>;
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Mint `amount` new tokens for `to`.
+		pub fn mint(origin: OriginFor<T>, to: T::AccountId, amount: Balance) -> DispatchResult {
+			let _anyone = ensure_signed(origin)?;
+
+			Balances::<T>::mutate(to, |b| *b = Some(b.unwrap_or(0) + amount));
+			TotalIssuance::<T>::mutate(|t| *t += amount);
+
+			Ok(())
+		}
+
+		/// Transfer exactly `amount` from `origin` to `to`. `origin` must exist, and `to` may not.
+		pub fn transfer(origin: OriginFor<T>, to: T::AccountId, amount: Balance) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			let sender_balance = Balances::<T>::get(&sender).ok_or("NonExistentAccount")?;
+			if sender_balance < amount {
+				return Err("notEnoughBalance".into())
+			}
+			let reminder = sender_balance - amount;
+
+			Balances::<T>::mutate(to, |b| *b = Some(b.unwrap_or(0) + amount));
+			Balances::<T>::insert(&sender, reminder);
+
+			Ok(())
+		}
+	}
+}
